219bb6899c36beaa02a3482e97ea764f60f07cd2
David Wettstein <dwettstein@gmail.com>
2017-06-02 10:05:38 +0200

    Export requestComfortboxApi for reusing it in all nodes

--- display-text/display-text.js
+++ display-text/display-text.js
@@ -1,8 +1,7 @@
 module.exports = function(RED) {
   'use strict';
 
-  var http = require('http');
-  var https = require('https');
+  var requestComfortboxApi = require('../lib/requestComfortboxApi.js');
 
   function DisplayTextNode(config) {
     RED.nodes.createNode(this, config);
@@ -16,72 +15,26 @@ module.exports = function(RED) {
     var node = this;
 
     node.on('input', function(msg) {
-      if (node.server) {
-        var payload = 'text=' + encodeURIComponent(msg.payload);
-
-        var options = {
-          hostname: node.server.host,
-          port: node.server.port,
-          path: '/api/ComfortBoxes/' + node.boxId + '/displayText' + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(payload),
-            'Accept': 'application/json'
-          }
-        }
-
-        if (node.server.host === 'localhost') {
-          // Ignore certificate errors if host is localhost.
-          options.rejectUnauthorized = false;
-        }
-
-        var req = (node.server.protocol === 'https' ? https : http).request(options, function(res) {
-          (node.return === "bin") ? res.setEncoding('binary') : res.setEncoding('utf8');
-          msg.statusCode = res.statusCode;
-          msg.headers = res.headers;
-          msg.responseUrl = res.responseUrl;
-          msg.payload = "";
-
-          res.on('data', function(chunk) {
-            msg.payload += chunk;
-          });
-
-          res.on('end', function() {
-            if (node.return === "bin") {
-              msg.payload = new Buffer(msg.payload, "binary");
-            } else if (node.return === "obj") {
-              try {
-                msg.payload = JSON.parse(msg.payload);
-              } catch(e) {
-                node.warn(RED._("httpin.errors.json-error"));
-              }
-            }
-            node.send(msg);
-            node.status({});
-          });
-        });
-
-        req.on('error', function(err) {
-          node.error(err, msg);
-          msg.payload = err.toString();
-          msg.statusCode = err.code;
-          node.send(msg);
-          node.status({fill: 'red', shape: 'ring', text: err.code});
-        });
-
-        if (payload) {
-          req.write(payload);
-        }
-
-        req.end();
+      var payload;
+      if (node.text) {
+        payload = 'text=' + encodeURIComponent(node.text);
       } else {
-        msg.payload = 'Error: No server config found! You have to select or create one.';
-        msg.statusCode = 400;
-        node.error(msg);
-        node.send(msg);
-        node.status({fill: 'red', shape: 'ring', text: msg.statusCode});
+        payload = 'text=' + encodeURIComponent(msg.payload);
       }
+
+      var options = {
+        hostname: node.server.host,
+        port: node.server.port,
+        path: '/api/ComfortBoxes/' + node.boxId + '/displayText' + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(payload),
+          'Accept': 'application/json'
+        }
+      }
+
+      requestComfortboxApi(msg, node, options, payload);
     });
 
     node.on("close", function() {

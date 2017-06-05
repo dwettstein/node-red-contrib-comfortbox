be3165655eeca075fc9bdba2aa33ee4282446a7f
David Wettstein <dwettstein@gmail.com>
2017-06-05 21:31:55 +0200

    Add API calls for each configure-box tab

--- configure-box/configure-box.js
+++ configure-box/configure-box.js
@@ -31,54 +31,199 @@ module.exports = function(RED) {
 
     node.on('input', function(msg) {
       node.status({});
+      var responseMsgs = [];
 
-      var doPatchBox = false;
+      // ********************************************************************************
+      // Update API information (PATCH /api/ComfortBoxes/:id)
+      // ********************************************************************************
+      var doBoxPatch = false;
       var comfortbox = {};
       if (!node.device || node.boxName !== node.device.name) {
         comfortbox.name = node.boxName;
-        doPatchBox = true;
+        doBoxPatch = true;
       }
       if (!node.device || node.particleId !== node.device.particleId) {
         comfortbox.particleId = node.particleId;
-        doPatchBox = true;
+        doBoxPatch = true;
       }
       if (!node.device || (node.labels && node.labels.length > 0 && JSON.stringify(node.labels) !== JSON.stringify(node.device.labels))) {
         comfortbox.labels = node.labels;
-        doPatchBox = true;
+        doBoxPatch = true;
       }
 
-      if (doPatchBox) {
-        var payload = JSON.stringify(comfortbox);
-        var options = {
+      RED.log.info('Execute box patch? ' + doBoxPatch);
+      if (doBoxPatch) {
+        var boxPatchPayload = JSON.stringify(comfortbox);
+        var boxPatchOptions = {
           hostname: node.server.host,
           port: node.server.port,
           path: '/api/ComfortBoxes/' + node.boxId + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
           method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(payload),
+            'Content-Length': Buffer.byteLength(boxPatchPayload),
             'Accept': 'application/json',
           },
         };
 
         if (node.server.host === 'localhost') {
           // Ignore certificate errors if host is localhost.
-          options.rejectUnauthorized = false;
+          boxPatchOptions.rejectUnauthorized = false;
         }
 
-        apiRequest(node.server.protocol, node.return, options, payload, function(res) {
+        apiRequest(node.server.protocol, node.return, boxPatchOptions, boxPatchPayload, function(res) {
           if (res && res.statusCode / 100 !== 2) {
             node.error(res);
             node.status({fill: 'red', shape: 'ring', text: res.statusCode});
           }
-          node.send(res);
+          responseMsgs += res.payload;
         });
-      } else {
-        msg.payload = 'The node had nothing to configure.';
-        node.send(msg);
       }
 
-      // TODO: Other configuration calls.
+      // ********************************************************************************
+      // Update MQTT information
+      // ********************************************************************************
+      RED.log.info('Execute MQTT update? ' + (node.mqttHost && node.mqttPort));
+      if (node.mqttHost && node.mqttPort) {
+        // e.g. host=localhost&port=1883
+        var mqttUpdatePayload = 'host=' + encodeURIComponent(node.mqttHost) + '&port=' + encodeURIComponent(node.mqttPort);
+        var mqttUpdateOptions = {
+          hostname: node.server.host,
+          port: node.server.port,
+          path: '/api/ComfortBoxes/' + node.boxId + '/setMqttHost' + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(mqttUpdatePayload),
+            'Accept': 'application/json',
+          },
+        };
+
+        if (node.server.host === 'localhost') {
+          // Ignore certificate errors if host is localhost.
+          mqttUpdateOptions.rejectUnauthorized = false;
+        }
+
+        apiRequest(node.server.protocol, node.return, mqttUpdateOptions, mqttUpdatePayload, function(res) {
+          if (res && res.statusCode / 100 !== 2) {
+            node.error(res);
+            node.status({fill: 'red', shape: 'ring', text: res.statusCode});
+          }
+          responseMsgs += res.payload;
+        });
+      }
+
+      // ********************************************************************************
+      // Update data interval
+      // ********************************************************************************
+      RED.log.info('Execute interval update? ' + node.dataInterval);
+      if (node.dataInterval) {
+        // e.g. interval=5
+        var intervalPayload = 'interval=' + encodeURIComponent(node.dataInterval);
+        var intervalOptions = {
+          hostname: node.server.host,
+          port: node.server.port,
+          path: '/api/ComfortBoxes/' + node.boxId + '/setInterval' + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(intervalPayload),
+            'Accept': 'application/json',
+          },
+        };
+
+        if (node.server.host === 'localhost') {
+          // Ignore certificate errors if host is localhost.
+          intervalOptions.rejectUnauthorized = false;
+        }
+
+        apiRequest(node.server.protocol, node.return, intervalOptions, intervalPayload, function(res) {
+          if (res && res.statusCode / 100 !== 2) {
+            node.error(res);
+            node.status({fill: 'red', shape: 'ring', text: res.statusCode});
+          }
+          responseMsgs += res.payload;
+        });
+      }
+
+      // ********************************************************************************
+      // Update working hours
+      // ********************************************************************************
+      RED.log.info('Execute worktime update? ' + node.worktime);
+      if (node.worktime) {
+        // e.g. worktime=08%3A00-17%3A00
+        var worktimePayload = 'worktime=' + encodeURIComponent(node.worktime);
+        var worktimeOptions = {
+          hostname: node.server.host,
+          port: node.server.port,
+          path: '/api/ComfortBoxes/' + node.boxId + '/setWorktime' + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(worktimePayload),
+            'Accept': 'application/json',
+          },
+        };
+
+        if (node.server.host === 'localhost') {
+          // Ignore certificate errors if host is localhost.
+          worktimeOptions.rejectUnauthorized = false;
+        }
+
+        apiRequest(node.server.protocol, node.return, worktimeOptions, worktimePayload, function(res) {
+          if (res && res.statusCode / 100 !== 2) {
+            node.error(res);
+            node.status({fill: 'red', shape: 'ring', text: res.statusCode});
+          }
+          responseMsgs += res.payload;
+        });
+      }
+
+      // ********************************************************************************
+      // doShowDataUpdate
+      // ********************************************************************************
+      RED.log.info('Execute show data regularly update? ' + node.doShowDataRegularly);
+      if (node.doShowDataRegularly) {
+        // e.g. doShowDataRegularly=true
+        var showDataPayload = 'doShowDataRegularly=' + encodeURIComponent(node.doShowDataRegularly);
+        var showDataOptions = {
+          hostname: node.server.host,
+          port: node.server.port,
+          path: '/api/ComfortBoxes/' + node.boxId + '/setShowDataRegularly' + (node.server.accessToken ? '?access_token=' + node.server.accessToken : ''),
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(showDataPayload),
+            'Accept': 'application/json',
+          },
+        };
+
+        if (node.server.host === 'localhost') {
+          // Ignore certificate errors if host is localhost.
+          showDataOptions.rejectUnauthorized = false;
+        }
+
+        apiRequest(node.server.protocol, node.return, showDataOptions, showDataPayload, function(res) {
+          if (res && res.statusCode / 100 !== 2) {
+            node.error(res);
+            node.status({fill: 'red', shape: 'ring', text: res.statusCode});
+          }
+          responseMsgs += res.payload;
+        });
+      }
+
+      // ********************************************************************************
+      // Send node response after 2 seconds (not a very good solution, I know...)
+      // ********************************************************************************
+      setTimeout(function() {
+        if (responseMsgs.length === 0) {
+          msg.payload = 'The node had nothing to configure.';
+          node.send(msg);
+        } else {
+          msg.payload = JSON.stringify({responses: responseMsgs});
+          node.send(msg);
+        }
+      }, 2000);
     });
 
     node.on('close', function() {
